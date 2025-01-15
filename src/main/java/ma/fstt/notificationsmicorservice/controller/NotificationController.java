6a9e82dc86c9c9ccda6c1ea416bc74e4322d28c4
Bihi-06 <aitsaid.ayoub@etu.uae.ac.ma>
2025-01-15 02:20:57 +0100

    update

--- src/main/java/ma/fstt/notificationsmicorservice/controller/NotificationController.java
+++ src/main/java/ma/fstt/notificationsmicorservice/controller/NotificationController.java
@@ -1,9 +1,10 @@
 package ma.fstt.notificationsmicorservice.controller;
 
 
-import ma.fstt.notificationsmicorservice.dto.NotificationRequest;
+import com.banque.events.dto.NotificationRequest;
 import ma.fstt.notificationsmicorservice.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -22,5 +23,17 @@ public class NotificationController {
                 request.getContent()
         );
         return "Notification sent!";
+    }
+
+    @PostMapping("/accountCreated")
+    public ResponseEntity<Void> sendNotificationAccountCreated(@RequestBody NotificationRequest request){
+        notificationService.sendNotification(
+                request.getType(),
+                request.getRecipientEmail(),
+                request.getSubject(),
+                request.getContent()
+        );
+
+        return ResponseEntity.ok().build();
     }
 }
